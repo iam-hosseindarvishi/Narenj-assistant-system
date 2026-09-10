00272165238cd9e4a531f48af3a3a0a5848a820b
hossein <iam.hosseindarvishi@gmail.com>
2026-09-10 22:50:59 +0330

    refactor: remove individual layer pages from sidebar, only wizard remains

--- src/renderer/App.vue
+++ src/renderer/App.vue
@@ -25,10 +25,6 @@
         <DashboardView v-if="activeView === 'dashboard'" />
         <ImportView v-else-if="activeView === 'import'" />
         <ReconciliationWizardView v-else-if="activeView === 'wizard'" />
-        <Layer1View v-else-if="activeView === 'layer1'" />
-        <Layer2View v-else-if="activeView === 'layer2'" />
-        <Layer3View v-else-if="activeView === 'layer3'" />
-        <Layer4View v-else-if="activeView === 'layer4'" />
         <ManualView v-else-if="activeView === 'manual'" />
         <TemplatesView v-else-if="activeView === 'templates'" />
         <UsersView v-else-if="activeView === 'users'" />
@@ -47,16 +43,12 @@
 import { computed, onMounted, ref } from 'vue'
 import DashboardView from './views/DashboardView.vue'
 import ImportView from './views/ImportView.vue'
-import Layer1View from './views/Layer1View.vue'
-import Layer2View from './views/Layer2View.vue'
-import Layer3View from './views/Layer3View.vue'
-import Layer4View from './views/Layer4View.vue'
+import ReconciliationWizardView from './views/ReconciliationWizardView.vue'
 import ManualView from './views/ManualView.vue'
 import TemplatesView from './views/TemplatesView.vue'
 import UsersView from './views/UsersView.vue'
 import AuditView from './views/AuditView.vue'
 import ReportsView from './views/ReportsView.vue'
-import ReconciliationWizardView from './views/ReconciliationWizardView.vue'
 import LoginView from './views/LoginView.vue'
 import { useAuthStore } from './stores/auth'
 
@@ -66,8 +58,6 @@ const activeView = ref('dashboard')
 const navigation = [
   { key: 'dashboard', title: 'داشبورد' }, { key: 'import', title: 'ورود فایل' },
   { key: 'wizard', title: 'مغایرت‌یابی خودکار' },
-  { key: 'layer1', title: 'مغایرت پوز-بانک' }, { key: 'layer2', title: 'کارمزدها' },
-  { key: 'layer3', title: 'مغایرت بانک-حسابداری' }, { key: 'layer4', title: 'مغایرت ریز پوز' },
   { key: 'manual', title: 'تطبیق دستی' }, { key: 'templates', title: 'قالب‌ها' },
   { key: 'users', title: 'کاربران', adminOnly: true }, { key: 'audit', title: 'حسابرسی' },
   { key: 'reports', title: 'گزارش‌ها' }
